0196953d25fb201a80360349c60e3dc33008d7ab
shuja1497 <shujareshi1497@gmail.com>
2017-10-31 17:30:43 +0530

    nearbyplaces and data parsing completed

--- MapsNearbyPlaces/app/src/main/java/com/shuja1497/mapsnearbyplaces/DataParser.java
+++ MapsNearbyPlaces/app/src/main/java/com/shuja1497/mapsnearbyplaces/DataParser.java
@@ -2,10 +2,13 @@ package com.shuja1497.mapsnearbyplaces;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by shuja1497 on 10/31/17.
@@ -57,4 +60,42 @@ public class DataParser {
         }
             return googlePlacaMap;
         }
+
+    // will return a list of hashmap
+    private List<HashMap<String, String>> getPlaces(JSONArray jsonArray){
+
+        int count = jsonArray.length();
+        List<HashMap<String, String>> placesList = new ArrayList<>();
+        HashMap<String, String> placeMap = null;
+        for (int i = 0 ; i < count ; i ++){
+
+            try {
+                placeMap = getPlace((JSONObject)jsonArray.get(i));
+                placesList.add(placeMap);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return placesList;
+    }
+
+    //parse calls getPlaces . which then calls getPlace
+
+    public List<HashMap<String, String>> parse(String jsonData){
+
+        JSONArray jsonArray = null;
+        JSONObject jsonObject ;
+        try {
+            jsonObject = new JSONObject(jsonData);
+            jsonArray = jsonObject.getJSONArray("results");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return getPlaces(jsonArray);
+    }
+
+
 }
--- MapsNearbyPlaces/app/src/main/java/com/shuja1497/mapsnearbyplaces/GetNearbyPlacesData.java
+++ MapsNearbyPlaces/app/src/main/java/com/shuja1497/mapsnearbyplaces/GetNearbyPlacesData.java
@@ -1,10 +1,22 @@
 package com.shuja1497.mapsnearbyplaces;
 
 import android.os.AsyncTask;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by shuja1497 on 10/31/17.
@@ -33,8 +45,42 @@ public class GetNearbyPlacesData extends AsyncTask<Object, String, String>{
         return googlePlacesData;
     }
 
+
+
     @Override
     protected void onPostExecute(String s) {
-        super.onPostExecute(s);
+//        super.onPostExecute(s);
+        List<HashMap<String, String>> nearbyPlaceList = null;
+        DataParser parser = new DataParser();
+
+        nearbyPlaceList = parser.parse(s);
+
+        showNearbyPlaces(nearbyPlaceList);
+    }
+
+// shows all places in the list
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlacesList){
+
+        for (int i = 0 ; i<nearbyPlacesList.size();i++){
+
+            MarkerOptions markerOptions = new MarkerOptions();
+
+            HashMap<String , String> googlePlace = nearbyPlacesList.get(i);
+
+            String placeName  =googlePlace.get("place_name");
+            String vicinity  = googlePlace.get("vicinity");
+            double lat = Double.parseDouble(googlePlace.get("lat"));//google places retuens a string so parse
+            double lng = Double.parseDouble(googlePlace.get("lng"));
+
+            LatLng latLng = new LatLng(lat , lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName +":" + vicinity);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN));
+            googleMap.addMarker(markerOptions);
+
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            googleMap.animateCamera(CameraUpdateFactory.zoomBy(10));
+
+        }
     }
 }
