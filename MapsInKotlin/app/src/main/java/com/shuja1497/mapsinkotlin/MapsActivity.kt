56aea515eb2d7eeb22b2270f8b06a63bf68ede51
shuja1497 <shujareshi1497@gmail.com>
2018-03-25 22:48:31 +0530

    basic map ready

--- MapsInKotlin/app/src/main/java/com/shuja1497/mapsinkotlin/MapsActivity.kt
+++ MapsInKotlin/app/src/main/java/com/shuja1497/mapsinkotlin/MapsActivity.kt
@@ -1,7 +1,14 @@
 package com.shuja1497.mapsinkotlin
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,11 +16,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.places.GeoDataClient
+import com.google.android.gms.location.places.PlaceDetectionClient
+import com.google.android.gms.location.places.Places
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -39,5 +49,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val a = this.checkPermission(Context.LOCATION_SERVICE,0,1)
+        Toast.makeText(this, a.toString(), Toast.LENGTH_LONG).show()
+        mMap.isMyLocationEnabled = true
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isZoomGesturesEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isRotateGesturesEnabled = true
     }
 }
